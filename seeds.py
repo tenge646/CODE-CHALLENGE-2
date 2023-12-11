d7bef65a3621a87f5dfb233370057705cec11899
tenge646 <144627741+tenge646@users.noreply.github.com>
2023-12-11 12:40:51 +0300

    done with seed

--- seeds.py
+++ seeds.py
@@ -20,3 +20,21 @@ for _ in range(4):
 for _ in range(10):
     customer = Customer(first_name=fake.first_name(), last_name=fake.last_name())
     session.add(customer)    
+
+# Link customers and restaurants with random reviews
+restaurants = session.query(Restaurant).all()
+customers = session.query(Customer).all()
+
+for _ in range(15):  # Create 15 random reviews
+    restaurant = fake.random_element(restaurants)
+    customer = fake.random_element(customers)
+    review = Review(star_rating=fake.random_int(1, 5), restaurant=restaurant, customer=customer)
+    session.add(review)
+
+session.commit()
+
+# Print reviews for each restaurant
+for restaurant in restaurants:
+    print(f"Reviews for {restaurant.name}:")
+    for review in restaurant.reviews:
+        print(f"Review for {restaurant.name} by {review.customer.first_name} {review.customer.last_name}: {review.star_rating} stars")
