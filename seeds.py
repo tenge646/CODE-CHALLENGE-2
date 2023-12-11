715cf32cfb109967ffcf8946a36fa7732e7ca76e
tenge646 <144627741+tenge646@users.noreply.github.com>
2023-12-11 12:37:49 +0300

    SQLite database

--- seeds.py
+++ seeds.py
@@ -2,3 +2,11 @@ from faker import Faker
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base, Restaurant, Customer, Review
+
+# Create an SQLite database in memory
+engine = create_engine('sqlite:///database.sqlite')
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+fake = Faker()
